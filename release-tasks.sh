1f5d502a9485c065a1c2bdbb97ad4ebd7c9089e8
Israel Brizuela <israelbrizuela98@gmail.com>
2022-02-16 14:14:10 -0300

    correccion de release

--- release-tasks.sh
+++ release-tasks.sh
@@ -4,4 +4,4 @@ python manage.py loaddata initial_pais_data.json
 python manage.py loaddata initial_departamento_data.json
 python manage.py loaddata initial_distrito_data.json
 python manage.py loaddata initial_localidad_data.json
-python manage.py loaddata initial_tipoItemjson
+python manage.py loaddata initial_tipoItem_datajson
