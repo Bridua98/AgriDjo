d8d475af366933b91c30ccb69cad3cc4dd99dde2
Israel Brizuela <israelbrizuela98@gmail.com>
2022-02-16 14:30:54 -0300

    test

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py loaddata initial_pais_data.json
-python manage.py loaddata initial_departamento_data.json
-python manage.py loaddata initial_distrito_data.json
-python manage.py loaddata initial_localidad_data.json
-python manage.py loaddata initial_tipoItem_data.json
