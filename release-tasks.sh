3acbebd9572074e6d93a745f4732b09fcef1935b
Israel Brizuela <israelbrizuela98@gmail.com>
2022-02-16 15:30:22 -0300

    release para load de carga inicial

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py loaddata initial_pais_data.json
+python manage.py loaddata initial_tipoItem_data.json
+python manage.py loaddata initial_departamento_data.json
+python manage.py loaddata initial_distrito_data.json
+python manage.py loaddata initial_localidad_data.json
